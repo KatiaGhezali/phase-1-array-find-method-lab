1c309b2177b75d61ab8d3f49d3ff43526789ad09
Katia Ghezali <ghezalikatia@hotmail.com>
2023-08-13 16:28:28 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+
+
+
+function superbowlWin(record){
+    const win = record.find(function(record) {
+        return record.result === 'W';
+    });
+     
+    if(win){
+        return win.year
+    } else {
+        return undefined
+    }
+
+
+}
